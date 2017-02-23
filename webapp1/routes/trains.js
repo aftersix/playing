9c144fff2e86491f68f40c1e101dcf1e3616c0b6
aftersix <alex.arnow@gmail.com>
2017-02-23 18:57:10 -0500

    I want to show alex something

--- webapp1/routes/trains.js
+++ webapp1/routes/trains.js
@@ -1,26 +1,32 @@
+var Sugar = require('sugar');
+var request = require('request');
 
-var array = {
-    "sync_contact_list": [
-        {
-            "name": "c",
-            "number": "c1",
-            "email": "@c"
-        },
-        {
-            "name": "b",
-            "number": "b1",
-            "email": "@b"
-        },
-        {
-            "name": "a",
-            "number": "a1",
-            "email": "@a"
-        }
-    ]
-};
+
+var mbtaFeed; //needs to be a universal variable to pass outside of the 
+
+
+request('http://developer.mbta.com/lib/RTCR/RailLine_12.json', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+      mbtaFeed = JSON.parse(body);
+	 
+	 //format the timestamp into something readable
+	 for (var i=0; i<mbtaFeed['Messages'].length; i++){
+		 
+		mbtaFeed['Messages'][i]['Scheduled'] = Sugar.Date.format(new Date(mbtaFeed['Messages'][i]['Scheduled']*1000),  '{hh}:{mm}');
+		 
+	 }
+	 
+	 
+	 
+	 //for testing - print the array of mbta data feeds
+	 console.log(mbtaFeed);
+  }
+})
+  
+
 
 
 exports.trains = function(req,res) {
-res.render('trains', {title:'trains' , array:array });
+res.render('trains', {title:'trains' , mbtaFeed:mbtaFeed });
 
 };
