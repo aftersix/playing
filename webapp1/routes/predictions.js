c9375601681ba6a150685b646f9e37321216b767
aftersix <alex.arnow@gmail.com>
2017-03-06 10:47:27 -0500

    updated lateness notifications

--- webapp1/routes/predictions.js
+++ webapp1/routes/predictions.js
@@ -13,6 +13,8 @@ console.log("ran predictions.js");
 
 exports.predictions = function(req,res) {
 
+
+
 var salemSchedule = {table: [] };
 var trainAlerts;
 
@@ -36,12 +38,19 @@ var trainAlerts;
 							for (var l=0; l<direction['trip'].length; l++){
 								//console.log(direction['trip'][l]['trip_name']);
 								
+								var difference = direction['trip'][l]['sch_arr_dt'] - direction['trip'][l]['pre_dt'];
+								difference = Math.abs(difference);
+								
+								
 								direction['trip'][l]['sch_arr_dt'] = Sugar.Date.format(new Date(direction['trip'][l]['sch_arr_dt']*1000),  '{hh}:{mm}');
 								//console.log("Scheduled at: " + direction['trip'][l]['sch_arr_dt']);
 								direction['trip'][l]['pre_dt'] = Sugar.Date.format(new Date(direction['trip'][l]['pre_dt']*1000),  '{hh}:{mm}');
+								//console.log("Predicted at: " + direction['trip'][l]['sch_arr_dt']);
 								
-								salemSchedule.table.push({train_direction: route['direction'][k]['direction_name'], train_name: direction['trip'][l]['trip_name'], scheduleTime: direction['trip'][l]['sch_arr_dt'],predictedTime:direction['trip'][l]['pre_dt']});
-								//console.log(salemSchedule);
+								console.log(difference);
+								
+								salemSchedule.table.push({train_direction: route['direction'][k]['direction_name'], train_name: direction['trip'][l]['trip_name'], scheduleTime: direction['trip'][l]['sch_arr_dt'],predictedTime:direction['trip'][l]['pre_dt'],difference:difference});
+								console.log(salemSchedule);
 								
 								
 							}
@@ -65,7 +74,7 @@ var trainAlerts;
 		  if (!error && response.statusCode == 200) {
 				for (var i=0; i<trainAlert['alerts'].length; i++){  
 					
-					console.log(trainAlert['alerts'][i]['short_header_text']);
+					//console.log(trainAlert['alerts'][i]['short_header_text']);
 				 }  
 		  }
 		trainAlerts = trainAlert;  
