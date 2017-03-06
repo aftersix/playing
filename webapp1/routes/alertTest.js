67627104eaed6647444934690b91ba1dbdc58409
aftersix <alex.arnow@gmail.com>
2017-03-06 09:34:08 -0500

    updated package.json file

--- webapp1/routes/alertTest.js
+++ webapp1/routes/alertTest.js
@@ -6,38 +6,10 @@ request({
 		json: true
 		},
 
-		 function (error, response, jsonObject) {
+		 function (error, response, trainAlerts) {
 		  if (!error && response.statusCode == 200) {
-			 
-				for (var i=0; i<jsonObject['alerts'].length; i++){  
-					var note = jsonObject['alerts'][i]['short_header_text'];
-					console.log(note);
-					
-					
-					/* for (var j=0; j<note['route'].length; j++){
-						var route = note['route'][j];
-						
-						for (var k=0; k<route['direction'].length; k++){
-							var direction = route['direction'][k];
-							//console.log(route['direction'][k]['direction_name']);
-							for (var l=0; l<direction['trip'].length; l++){
-								//console.log(direction['trip'][l]['trip_name']);
-								
-								direction['trip'][l]['sch_arr_dt'] = Sugar.Date.format(new Date(direction['trip'][l]['sch_arr_dt']*1000),  '{hh}:{mm}');
-								//console.log("Scheduled at: " + direction['trip'][l]['sch_arr_dt']);
-								direction['trip'][l]['pre_dt'] = Sugar.Date.format(new Date(direction['trip'][l]['pre_dt']*1000),  '{hh}:{mm}');
-								
-								salemSchedule.table.push({train_direction: route['direction'][k]['direction_name'], train_name: direction['trip'][l]['trip_name'], scheduleTime: direction['trip'][l]['sch_arr_dt'],predictedTime:direction['trip'][l]['pre_dt']});
-								//console.log(salemSchedule);
-								
-								
-							}
-						}
-					} */
-				  }  
+				for (var i=0; i<trainAlerts['alerts'].length; i++){  
+				 }  
 		  }
-		 
-		
-		
 		}
 );
